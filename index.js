5a0e6157d3b098c03750bfafd8b178b0605c2d2e
Sudharsan1402 <112768666+Sudharsan1402@users.noreply.github.com>
2025-06-22 16:52:54 +0530

    Resolve merge conflict

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ const port = process.env.PORT || 3000;
 dotenv.config();
 
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: String(process.env.DB_PASSWORD),
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
-db.connect();
 
-console.log(String(process.env.DB_PASSWORD));
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
